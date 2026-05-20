4b01c00faa1aeed1b9d605b1c8cedff1f15c13d4
enes algan <Enes.Algan@turkiyefinans.com.tr>
2026-05-20 11:22:29 +0300

    Update ApiKeyConfig.kt

--- src/main/kotlin/org/jetbrains/plugins/designer/config/ApiKeyConfig.kt
+++ src/main/kotlin/org/jetbrains/plugins/designer/config/ApiKeyConfig.kt
@@ -6,13 +6,13 @@ import java.util.Base64
 
 object ApiKeyConfig {
 
-    private const val ENCRYPTED_API_KEY = "AAS7kL5gC2/4qTcCcEklyGNnHcaJMpYuiFhVXC46lxCPSUmTRFULEVV8NIXLrZHr0LSrdiXsQE+WIXOetVZuteNLw0OddHAq4CRwKp9DWM3QbYXjgUsW+ylbMNBhvVwRjVkUDIklQLAx0GEhyXy5Ihw9nz1KtfaeTh+FIKST0TnkU0l6Y1GUDqpBl7YZ2t3oZym6bMJWad5qvYM8SNtV724YxS7twYlkzB0iDBuDpD8="
+    private const val ENCRYPTED_API_KEY = "AAS7kL5gC2/+WIXOetVZuteNLw0OddHAq4CRwKp9DWM3QbYXjgUsW+ylbMNBhvVwRjVkUDIklQLAx0GEhyXy5Ihw9nz1KtfaeTh+FIKST0TnkU0l6Y1GUDqpBl7YZ2t3oZym6bMJWad5qvYM8SNtV724YxS7twYlkzB0iDBuDpD8="
 
     private const val ENCRYPTION_KEY = "ensalgn761234567"
 
     fun getApiKey(): String {
         return try {
-            "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"
+            "sk-proj-Xcw2G3Y0BMh7mmcukk3pjlhEl53s-"
          //   decrypt(ENCRYPTED_API_KEY, ENCRYPTION_KEY)
         } catch (e: Exception) {
             throw IllegalStateException("API key configuration error", e)
